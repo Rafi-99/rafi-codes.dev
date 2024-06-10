bfe52cc647f9d92bb3e9736959acb81360792081
Rafi-99 <rafi.md.2018@gmail.com>
2024-06-10 03:48:40 -0500

    Updated Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { FaFacebookSquare, FaGithubSquare, FaInstagramSquare, FaLinkedin } from 'react-icons/fa';
+import { FaSquareGithub, FaLinkedin } from 'react-icons/fa6';
 import styles from '../styles/components/Footer.module.css';
 
 export default function Footer() {
@@ -7,9 +7,7 @@ export default function Footer() {
         <footer className={styles.footer}>
             <small>Designed by Rafi | &copy; {new Date().getFullYear()}</small>
             <div className={styles.icons}>
-                <Link href='https://www.facebook.com/rafi2022/' rel='noopener noreferrer' target='_blank' aria-label='Link to Rafi&apos;s Facebook'><FaFacebookSquare /></Link>
-                <Link href='https://www.github.com/Rafi-99/' rel='noopener noreferrer' target='_blank' aria-label='Link to Rafi&apos;s GitHub'><FaGithubSquare /></Link>
-                <Link href='https://www.instagram.com/runner_rafi/' rel='noopener noreferrer' target='_blank' aria-label='Link to Rafi&apos;s Instagram'><FaInstagramSquare /></Link>
+                <Link href='https://www.github.com/Rafi-99/' rel='noopener noreferrer' target='_blank' aria-label='Link to Rafi&apos;s GitHub'><FaSquareGithub /></Link>
                 <Link href='https://www.linkedin.com/in/rafi2018/' rel='noopener noreferrer' target='_blank' aria-label='Link to Rafi&apos;s LinkedIn'><FaLinkedin /></Link>
             </div>
         </footer>
